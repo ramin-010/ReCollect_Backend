caac79d8bd97d477203e403ee33f0d5179d420c0
ramin-010 <raminchoudhary737@gmail.com>
2026-02-06 01:16:35 +0530

    implemented cursor broadcasting as will worked on perfomance enhancemet

--- src/collaboration/hocuspocus.ts
+++ src/collaboration/hocuspocus.ts
@@ -140,7 +140,7 @@ function getRandomColor(userId: string): string {
   const colors = [
     '#F44336', '#E91E63', '#9C27B0', '#673AB7', '#3F51B5',
     '#2196F3', '#03A9F4', '#00BCD4', '#009688', '#4CAF50',
-    '#8BC34A', '#CDDC39', '#FFC107', '#FF9800', '#FF5722',
+    '#dcf1c4ff', '#CDDC39', '#FFC107', '#FF9800', '#FF5722',
   ];
   
   let hash = 0;
